e2a9bd1fb0fe946dc5e528eba24300b9bbe5e732
Malfredsson <hannes.dejke@student.tabyenskilda.se>
2025-04-28 21:31:59 +0200

    Lade till snake

--- docs/js/spel.js
+++ docs/js/spel.js
@@ -1,3 +1,5 @@
+var snakeIsOn = false;
+
 function changeStatus (game) {
     let newStatus = (game.dataset.isActive != "true");
     game.dataset.isActive = newStatus;
@@ -8,4 +10,29 @@ function changeStatus (game) {
     }
     game.children[0].style.backgroundColor = bgColor;
     
+}
+
+function switchSnake () {
+    snakeIsOn = !snakeIsOn;
+
+    if (snakeIsOn) {
+        document.getElementById("snake-canvas-holder").style.display = "flex";
+        setup();
+    } else {
+        document.getElementById("snake-canvas-holder").style.display = "none";
+        game.over();
+    }
+}
+
+function updateHighscore () {
+    
+    let scores = document.querySelectorAll(".score");
+    let highscore = scores[0];
+    for (let i = 1; i < scores.length; i++) {
+        scores[i].style.color = "cyan";
+        if (parseFloat(scores[i].getAttribute("data-value")) > parseFloat(highscore.getAttribute("data-value"))) {
+            highscore = scores[i];
+        }
+    }
+    highscore.style.color = "gold";
 }
